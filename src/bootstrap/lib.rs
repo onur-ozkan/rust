48d2d155190a96f73a104b697bf9bbac8f0af86f
ozkanonur <work@onurozkan.dev>
2023-02-08 20:53:25 +0300

    move `min_config` related options from `config` module
    Signed-off-by: ozkanonur <work@onurozkan.dev>

--- src/bootstrap/lib.rs
+++ src/bootstrap/lib.rs
@@ -88,6 +88,7 @@ mod job {
 pub use crate::builder::PathSet;
 use crate::cache::{Interned, INTERNER};
 pub use crate::config::Config;
+pub use crate::flags::Flags;
 pub use crate::flags::Subcommand;
 pub use crate::min_config::MinimalConfig;
 
