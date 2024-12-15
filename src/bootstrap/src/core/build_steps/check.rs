1d23cecd40b80b94d00d734a6c14038f6d461673
onur-ozkan <work@onurozkan.dev>
2024-12-15 14:06:09 +0300

    improve `check::Std` handling
    Signed-off-by: onur-ozkan <work@onurozkan.dev>

--- src/bootstrap/src/core/build_steps/check.rs
+++ src/bootstrap/src/core/build_steps/check.rs
@@ -2,6 +2,7 @@
 
 use std::path::PathBuf;
 
+use crate::core::build_steps::compile;
 use crate::core::build_steps::compile::{
     add_to_sysroot, run_cargo, rustc_cargo, rustc_cargo_env, std_cargo, std_crates_for_run_make,
 };
@@ -41,7 +42,8 @@ impl Step for Std {
     const DEFAULT: bool = true;
 
     fn should_run(run: ShouldRun<'_>) -> ShouldRun<'_> {
-        run.crate_or_deps("sysroot").path("library")
+        let stage = run.builder.top_stage;
+        run.crate_or_deps("sysroot").path("library").default_condition(stage != 0)
     }
 
     fn make_run(run: RunConfig<'_>) {
@@ -59,6 +61,12 @@ impl Step for Std {
 
         let target = self.target;
         let compiler = builder.compiler(builder.top_stage, builder.config.build);
+
+        if builder.top_stage == 0 {
+            // Reuse the beta compiler's libstd
+            builder.ensure(compile::Std::new(compiler, target));
+            return;
+        }
 
         let mut cargo = builder::Cargo::new(
             builder,
