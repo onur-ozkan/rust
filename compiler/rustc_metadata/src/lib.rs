128089648b1e272318c33992fa25a92243918ace
onur-ozkan <work@onurozkan.dev>
2025-03-12 16:37:25 +0300

    handle cfg bootstrap on compiler and compiler tools
    Signed-off-by: onur-ozkan <work@onurozkan.dev>

--- compiler/rustc_metadata/src/lib.rs
+++ compiler/rustc_metadata/src/lib.rs
@@ -1,5 +1,6 @@
 // tidy-alphabetical-start
 #![allow(internal_features)]
+#![cfg_attr(bootstrap, feature(extract_if))]
 #![doc(html_root_url = "https://doc.rust-lang.org/nightly/nightly-rustc/")]
 #![doc(rust_logo)]
 #![feature(coroutines)]
